085c9c40ccbbd3f9bc427f44d6b02314bd63df76
= <=>
2022-02-02 15:32:05 +0530

    Mongo Db Database connected

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+// connect Db
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>res.send('API Running..'));
+app.get('/', (req, res) => res.send('API Running..'));
 
-app.listen(PORT, ()=> console.log(`Server is listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
