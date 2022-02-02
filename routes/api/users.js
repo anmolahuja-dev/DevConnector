56eea25212d648fc9e3c11aecc28580149d2f4a7
= <=>
2022-02-03 02:16:33 +0530

    Added User model using mongoose and added POST request to register users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route           GET api/users
-// @description     Test Route
+// @route           POST api/users
+// @description     Register User
 // @access          Public
-router.get('/', (req, res) => res.send('User Route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check(
+      'password',
+      'Please Enter a password with six or more characters'
+    ).isLength(6),
+    check('email', 'Enter a valid email address').isEmail(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    res.send('User Route');
+  }
+);
 module.exports = router;
