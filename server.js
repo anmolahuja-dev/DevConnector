3193e0548b95498f3bf64af69d94d7d8e8466a51
= <=>
2022-02-02 15:51:02 +0530

    Routes initialized

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const app = express();
 // connect Db
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-
 app.get('/', (req, res) => res.send('API Running..'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
