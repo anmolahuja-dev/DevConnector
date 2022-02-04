944976275fbe262b99f05f24c141a2806085477e
= <=>
2022-02-04 22:33:10 +0530

    Get Current user added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route           GET api/profile
-// @description     Test Route
-// @access          Public
-router.get('/', (req, res) => res.send('Profile Route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route           GET api/profile/me
+// @description     Get current user's Profile 
+// @access          Private
+router.get('/me',auth,async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id}).populate('user',['name','avatar']); //to add name and avatar attribute from user model, here we are setting user attribute of profile model with the authenticated user id
+
+        if(!profile){
+            return res.status(400).json({msg:'There is no profile for this user'});
+        }
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.error(500).send('Server Error');
+    }
+});
 module.exports = router;
