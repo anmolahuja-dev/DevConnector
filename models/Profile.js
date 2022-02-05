502a7e91c0af320045adba45c52a1de4911f9a74
= <=>
2022-02-05 22:26:41 +0530

    Create and update profile route added

--- models/Profile.js
+++ models/Profile.js
@@ -4,114 +4,113 @@ const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    handle: {
-      type: String,
-      required: true,
-      max: 40,
-    },
-    company: {
-      type: String,
-    },
-    website: {
-      type: String,
-    },
-    location: {
-      type: String,
-    },
-    status: {
-      type: String,
-      required: true,
-    },
-    skills: {
-      type: [String],
-      required: true,
-    },
-    bio: {
-      type: String,
-    },
-    githubusername: {
-      type: String,
-    },
-    experience: [
-      {
-        title: {
-          type: String,
-          required: true,
-        },
-        company: {
-          type: String,
-          required: true,
-        },
-        location: {
-          type: String,
-        },
-        from: {
-          type: Date,
-          required: true,
-        },
-        to: {
-          type: Date,
-        },
-        current: {
-          type: Boolean,
-          default: false,
-        },
-        description: {
-          type: String,
-        },
+  },
+  handle: {
+    type: String,
+    max: 40,
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
       },
-    ],
-    education: [
-      {
-        school: {
-          type: String,
-          required: true,
-        },
-        degree: {
-          type: String,
-          required: true,
-        },
-        fieldofstudy: {
-          type: String,
-          required: true,
-        },
-        from: {
-          type: Date,
-          required: true,
-        },
-        to: {
-          type: Date,
-        },
-        current: {
-          type: Boolean,
-          default: false,
-        },
-        description: {
-          type: String,
-        },
+      company: {
+        type: String,
+        required: true,
       },
-    ],
-    social: {
-      youtube: {
+      location: {
         type: String,
       },
-      twitter: {
-        type: String,
+      from: {
+        type: Date,
+        required: true,
       },
-      facebook: {
-        type: String,
+      to: {
+        type: Date,
       },
-      linkedin: {
-        type: String,
+      current: {
+        type: Boolean,
+        default: false,
       },
-      instagram: {
+      description: {
         type: String,
       },
     },
-    date: {
-      type: Date,
-      default: Date.now,
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
     },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
   },
 });
 
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,23 +4,108 @@ const auth = require('../../middleware/auth');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
 
 // @route           GET api/profile/me
-// @description     Get current user's Profile 
+// @description     Get current user's Profile
 // @access          Private
-router.get('/me',auth,async (req, res) => {
-    try {
-        const profile = await Profile.findOne({user: req.user.id}).populate('user',['name','avatar']); //to add name and avatar attribute from user model, here we are setting user attribute of profile model with the authenticated user id
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    ); //to add name and avatar attribute from user model, here we are setting user attribute of profile model with the authenticated user id
 
-        if(!profile){
-            return res.status(400).json({msg:'There is no profile for this user'});
-        }
-
-        res.json(profile);
-
-    } catch (error) {
-        console.error(error.message);
-        res.error(500).send('Server Error');
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.error(500).send('Server Error');
+  }
 });
+
+// @route           POST api/profile
+// @description     Create or update User's Profile user's Profile
+// @access          Private
+
+// here we need two middlewares auth and validation middleware so we use [auth,[validation checks]]
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is requried').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
+
+    // build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills)
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+
+    // Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      // if the profile exist then update the profile
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
